1d0be13b6af24303c10c4422c4a0c26abab0cdd1
wzp <Wuzhipeng37@163.com>
2022-10-19 11:24:48 +0800

    first commit

---
+++ src/main/java/com/wzp/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wzp.git;
+
+/**
+ * @author wzp
+ * @date 2022/10/19 11:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
